1312738b3c6536731af6aa2428fbd7ef6c7c9879
Baird Castleberry <baird@bairdcastleberry.com>
2014-07-21 15:51:48 -0500

    initial

---
+++ gen/form.php
@@ -0,0 +1,15 @@
+ <html> 
+ <head>
+ <title>Submit Data to PDF via form</title>
+ </head> 
+ <body> 
+  
+ <form action="genpdf.php" method="post">
+ 
+ Full Name:<input type="text" name="name" />
+ 
+ <input type="submit" />
+ 
+ </form> 
+ </body> 
+ </html> 
---
+++ gen/genpdf.php
@@ -0,0 +1,87 @@
+<?php
+
+//new buffer
+
+ob_start();
+
+require_once('../tcpdf/tcpdf.php');
+require_once('../FPDI-1.5.2/fpdi.php');
+
+// initiate FPDI
+
+$pdf = new FPDI();
+
+// Removes default header and footer (black line)
+
+$pdf->SetPrintHeader(false);
+$pdf->SetPrintFooter(false);
+
+// set document information
+
+$pdf->SetCreator('URI');
+$pdf->SetAuthor('URI');
+$pdf->SetTitle('URI');
+$pdf->SetSubject('URI');
+
+// add a page
+
+$pdf->AddPage();
+
+// set the source
+
+$pdf->setSourceFile('../template.pdf');
+
+$pdf->SetFont("Times", "", 12);
+$pdf->SetTextColor(0,0,0);
+
+// Test Variable
+
+$name = 'Name McNameyson';
+
+// PAGE ONE TEXT
+
+//$pdf->MultiCell(80, 3, strtoupper($_POST['name']) .,, 0, 'L', 0, 1, '24', '68', true);
+
+$pdf->MultiCell(80, 3, strtoupper($name) .',', 0, 'L', 0, 1, '24', '68', true);
+
+// increment pages
+
+$tplidx = $pdf->importPage(1);
+for ($i = 1; $i < 4; $i++)
+{ 
+$tplidx = $pdf->ImportPage($i);
+
+// choose page to write
+ 
+$pdf->useTemplate($tplidx);
+$pdf->AddPage();
+
+// PAGE TWO TEXT
+
+                     if ($i==1) {
+
+$pdf->MultiCell(150, 3, "I, " . $_POST ['name'] . "string following variable", 1, 'L', 0, 1, '24', '36', true);
+
+                     }
+                     
+// PAGE THREE TEXT
+
+                     if ($i==2) {
+
+$pdf->MultiCell(80, 3, strtoupper($_POST ['name']), 1, 'L', 0, 1, '25', '62', true);
+$pdf->MultiCell(80, 3, strtoupper($_POST ['name']) . ", " . strtoupper($_POST ['name']) . " " .  strtoupper($_POST ['name']), 1, 'L', 0, 1, '25', '70', true);
+
+
+                     }
+
+}
+
+// buffer
+
+ob_end_clean();
+
+// output
+
+$pdf->Output('output.pdf', 'D');
+
+?>
